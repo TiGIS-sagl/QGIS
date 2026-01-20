e07bab09be36545bc859a0a1615c3396d0bddf2d
ludotosk <72620499+ludotosk@users.noreply.github.com>
2026-01-20 12:10:12 +0100

    Fix runned pre-commit on qgswfsprovider.cpp

--- src/providers/wfs/qgswfsprovider.cpp
+++ src/providers/wfs/qgswfsprovider.cpp
@@ -147,9 +147,9 @@ QgsWFSProvider::QgsWFSProvider( const QString &uri, const ProviderOptions &optio
       mValid = false;
       return;
     }
-    if (  mShared->mWKBType == Qgis::WkbType::CompoundCurve
-    || mShared->mWKBType == Qgis::WkbType::CircularString
-    || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
+    if ( mShared->mWKBType == Qgis::WkbType::CompoundCurve
+         || mShared->mWKBType == Qgis::WkbType::CircularString
+         || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
       mCapabilities |= Qgis::VectorProviderCapability::CircularGeometries;
     mThisTypenameFields = mShared->mFields;
     mLayerPropertiesListWhenNoSqlRequest = mShared->mLayerPropertiesList;
@@ -165,9 +165,9 @@ QgsWFSProvider::QgsWFSProvider( const QString &uri, const ProviderOptions &optio
   if ( mShared->mWKBType == Qgis::WkbType::Unknown && mShared->mURI.hasGeometryTypeFilter() && mShared->mCaps.supportsGeometryTypeFilters() )
   {
     mShared->mWKBType = mShared->mURI.geometryTypeFilter();
-    if (  mShared->mWKBType == Qgis::WkbType::CompoundCurve
-    || mShared->mWKBType == Qgis::WkbType::CircularString
-    || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
+    if ( mShared->mWKBType == Qgis::WkbType::CompoundCurve
+         || mShared->mWKBType == Qgis::WkbType::CircularString
+         || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
       mCapabilities |= Qgis::VectorProviderCapability::CircularGeometries;
     if ( mShared->mWKBType != Qgis::WkbType::Unknown )
     {
@@ -641,9 +641,9 @@ bool QgsWFSProvider::processSQL( const QString &sqlString, QString &errorMsg, QS
     {
       mShared->mGeometryAttribute = geometryAttribute;
       mShared->mWKBType = geomType;
-      if (  mShared->mWKBType == Qgis::WkbType::CompoundCurve
-            || mShared->mWKBType == Qgis::WkbType::CircularString
-            || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
+      if ( mShared->mWKBType == Qgis::WkbType::CompoundCurve
+           || mShared->mWKBType == Qgis::WkbType::CircularString
+           || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
         mCapabilities |= Qgis::VectorProviderCapability::CircularGeometries;
       mGeometryMaybeMissing = geometryMaybeMissing;
       mThisTypenameFields = fields;
@@ -918,9 +918,9 @@ void QgsWFSProvider::featureReceivedAnalyzeOneFeature( const QVector<QgsFeatureU
           }
         }
       }
-    if (  mShared->mWKBType == Qgis::WkbType::CompoundCurve
-    || mShared->mWKBType == Qgis::WkbType::CircularString
-    || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
+      if ( mShared->mWKBType == Qgis::WkbType::CompoundCurve
+           || mShared->mWKBType == Qgis::WkbType::CircularString
+           || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
         mCapabilities |= Qgis::VectorProviderCapability::CircularGeometries;
     }
   }
@@ -1907,9 +1907,9 @@ bool QgsWFSProvider::getCapabilities()
     }
   }
 
-    if (  mShared->mWKBType == Qgis::WkbType::CompoundCurve
-        || mShared->mWKBType == Qgis::WkbType::CircularString
-        || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
+  if ( mShared->mWKBType == Qgis::WkbType::CompoundCurve
+       || mShared->mWKBType == Qgis::WkbType::CircularString
+       || mShared->mWKBType == Qgis::WkbType::CurvePolygon )
     mCapabilities |= Qgis::VectorProviderCapability::CircularGeometries;
 
   if ( !foundLayer )
